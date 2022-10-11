ebad01f9610802e15c5a28b4ea80566110ffb2ef
jakobdannel <jakob.dannel@t-online.de>
2022-10-11 22:42:05 +0200

    Added line drawing

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,12 @@
 use image::{Rgb, RgbImage};
+extern crate line_drawing;
+use line_drawing::Bresenham;
 
 fn main() {
-    let img = RgbImage::new(500,500);
-    
+    let mut img = RgbImage::new(500,500);
+
+    for (x, y) in Bresenham::new((0, 0), (499, 499)) {
+        img.put_pixel(x as u32, y as u32, Rgb([255,255,255]));
+    }
     img.save("./output/output.png").expect("Writing image");
 }
